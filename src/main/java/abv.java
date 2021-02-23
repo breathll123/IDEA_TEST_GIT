d14aa7e735f27b39d93a1726986fc32391a09e0a
breathll123 <446477483@qq.com>
2021-02-23 21:04:31 +0800

    FIRST

---
+++ src/main/java/abv.java
@@ -0,0 +1,16 @@
+/**
+ * @Author LWS
+ * @Date 2021/2/23 + 8:38 下午
+ */
+public class abv {
+    public static void main(String[] args) {
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+        System.out.println("i love you ");
+
+
+
+    }
+}
